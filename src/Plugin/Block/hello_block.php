7c59f6769360b13005ee42c8848a3ff3dcf47479
Lisa <lisa.dreier@fau.de>
2018-07-22 23:34:28 +0200

    No, it doesn't

--- src/Plugin/Block/hello_block.php
+++ src/Plugin/Block/hello_block.php
@@ -23,19 +23,5 @@ class hello_block extends BlockBase {
       '#markup' => $this->t('Hello, World!'),
     );
   }
-  
-  
-  (function ($) {
-  'use strict';
-  Drupal.behaviors.example_timeline = {
-    //apply everything here once per element
-    attach: function (context, settings) {
-     $('input.myCustomBehavior', context).once('myCustomBehavior', function () {
-      // Apply the myCustomBehaviour effect to all the elements only once.
-         alert('Ein Zufallsereignis ist geschehen! Im hello_block');
-    });
-    }
-  };
-})(jQuery);
 
 }
