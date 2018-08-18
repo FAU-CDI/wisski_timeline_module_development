93333218a686c1b5628caa0af98def3144b088eb
root <root@server-studidev.gnm.de>
2018-08-18 13:28:03 +0200

    First Layout + Settings similar to Linkblock

--- src/Plugin/Block/hello_block.php
+++ src/Plugin/Block/hello_block.php
@@ -4,6 +4,11 @@ namespace Drupal\wisski_timeline\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\wisski_pathbuilder\Entity\WisskiPathbuilderEntity as Pathbuilder;
+use Drupal\wisski_salz\AdapterHelper;
+use Drupal\wisski_core\Entity\WisskiEntity;
+
 
 /**
  * Provides a 'Hello' Block.
@@ -21,19 +26,262 @@ class hello_block extends BlockBase {
    */
   public function build() {
     $config = $this->getConfiguration();
-    if (!empty($config['hello_block_name'])) {
-      $name = $config['hello_block_name'];
+    
+#    if (isset($config['better_lb']) && $config['better_lb']) {
+#      return $this->betterBuild();
+#    }
+
+#  dpm($config);
+
+    // check if we ask for multiple pbs and multiple adapters.
+    if(isset($config['multi_pb']))
+      $multimode = $config['multi_pb'];
+    else
+      $multimode = FALSE;
+
+    $out = array();
+
+    // what individual is queried?
+    $individualid = \Drupal::routeMatch()->getParameter('wisski_individual');
+    // if we get an entity, just use the id for the inner functions
+    if ($individualid instanceof \Drupal\wisski_core\Entity\WisskiEntity) $individualid = $individualid->id();
+
+    // if we have no - we're done here.
+    if(empty($individualid)) {
+      return $out;
     }
-    else {
-      $name = $this->t('to no one');
+
+    if(isset($config['pathbuilder']))
+      $linkblockpbid = $config['pathbuilder'];
+    else
+      $linkblockpbid = NULL;
+
+    if(empty($linkblockpbid)) {
+      drupal_set_message("No Pathbuilder is specified for Linkblock.", "error");
+      return $out;
     }
+
+    $pb = \Drupal\wisski_pathbuilder\Entity\WisskiPathbuilderEntity::load($linkblockpbid);
+
+    if(empty($pb)) {
+      drupal_set_message("Something went wrong while loading data for Linkblock. No Pb was found!", "error");
+      return $out;
+    }
+
+
+    // load all pbs only in multimode
+    if($multimode)
+      $pbs = \Drupal\wisski_pathbuilder\Entity\WisskiPathbuilderEntity::loadMultiple();
+    else
+      $pbs = array($pb);
+    
+    $dataout = array();
+
+    // load all adapters here so we load them only once...    
+    // in case of multimode, select all adapters
+    if($multimode) 
+      $adapters = \Drupal\wisski_salz\Entity\Adapter::loadMultiple(); //entity_load_multiple('wisski_salz_adapter');            
+    else // else use just the given one.
+      $adapters = array(\Drupal\wisski_salz\Entity\Adapter::load($pb->getAdapterId()));
+    
+    foreach($pbs as $datapb) {
+    
+      // skip the own one only in multimode
+      if($pb == $datapb && $multimode)
+        continue;
+
+
+      // get the bundleid for the individual    
+      $bundleid = $datapb->getBundleIdForEntityId($individualid);
+      
+      // get the group for the bundleid
+      $groups = $datapb->getGroupsForBundle($bundleid);
+
+      // iterate all groups    
+      foreach($groups as $group) {
+        $linkgroup = \Drupal\wisski_pathbuilder\Entity\WisskiPathEntity::load($group->id());
+
+
+        // if there is any
+        if(!empty($linkgroup)) {
+          $allpbpaths = $pb->getPbPaths();
+          $pbtree = $pb->getPathTree();
+
+          // if there is nothing, then don't show up!
+          if(empty($allpbpaths) || !isset($allpbpaths[$linkgroup->id()]))
+//            return;
+// do not return! this leads to other pbs being unable to answer!
+            continue;
+
+          $pbarray = $allpbpaths[$linkgroup->id()];
+
+          // for every path in there, load something
+          foreach($pbtree[$linkgroup->id()]['children'] as $child) {
+            $childid = $child['id'];
+
+            // better catch these.            
+            if(empty($childid) || ( isset($allpbpaths[$childid]) && $allpbpaths[$childid]['enabled'] == 0 ) )
+              continue;
+
+            $path = \Drupal\wisski_pathbuilder\Entity\WisskiPathEntity::load($childid);
+#drupal_set_message("child: " . serialize($childid));            
+#            $adapters = \Drupal\wisski_salz\Entity\WisskiSalzAdapter
+
+#              dpm($adapters);
+
+            foreach($adapters as $adapter) {
+              $engine = $adapter->getEngine();
+
+              // get the data for this specific thing
+              $tmpdata = $engine->pathToReturnValue($path, $pb, $individualid, 0, 'target_id', FALSE);
+#              drupal_set_message("path: " . serialize($path));
+
+#              dpm($tmpdata, "tmp");
+              if(!empty($tmpdata)) {
+                $dataout[$path->id()]['path'] = $path;
+
+                $dataout[$path->id()]['adapter'] = $adapter;
+
+                if(!isset($dataout[$path->id()]['data']))
+                  $dataout[$path->id()]['data'] = array();
+
+                $dataout[$path->id()]['data'] = array_merge($dataout[$path->id()]['data'], $tmpdata);
+              }
+            }
+
+          }
+
+        }
+        #dpm($linkgroup);
+      }
+    }
+
+
+    // cache for 2 seconds so subsequent queries seem to be fast
+#    if(!empty($dataout))  
+    $out[]['#cache']['max-age'] = 2;
+    // this does not work
+#    $out['#cache']['disabled'] = TRUE;
+#    $out[] = [ '#markup' => 'Time : ' . date("H:i:s"),];
+#    drupal_set_message(serialize($dataout));
+    $topBundles = array();
+    $set = \Drupal::configFactory()->getEditable('wisski_core.settings');
+    $only_use_topbundles = $set->get('wisski_use_only_main_bundles');
+
+    if($only_use_topbundles)
+      $topBundles = \Drupal\wisski_core\WisskiHelper::getTopBundleIds();
+
+    //Timelinegrundgeruest
+    $my_dom = $this->load_my_html('');
+
+    $out[] = array(
+      '#children' => $my_dom->saveHTML(),
+      '#attached' => array(
+        'library' => array(
+          'wisski_timeline/example_timeline',
+        ),
+      ),
+    );
+
+    //OUTput like in Linkblock
+    foreach($dataout as $pathid => $dataarray) {
+      $path = $dataarray['path'];
+      $adapter = $dataarray['adapter'];
+
+      if(empty($dataarray['data']))
+        continue;
+
+      $out[] = [ '#markup' => '<h3>' . $path->getName() . '</h3>'];
+
+      foreach($dataarray['data'] as $data) {
+
+        $url = NULL;
+
+        if(isset($data['wisskiDisamb']))
+          $url = $data['wisskiDisamb'];
+
+        if(!empty($url)) {
+
+          $entity_id = AdapterHelper::getDrupalIdForUri($url);
+
+          if(!empty($adapter))
+            $bundles = $adapter->getBundleIdsForEntityId($entity_id);
+          else
+            $bundles = NULL;
+
+          $bundle = NULL;
+          if($only_use_topbundles) {
+            $topbundletouse = array_intersect($bundles, $topBundles);
+            if(!empty($topbundletouse))
+              $bundle = current($topbundletouse);
+          } else {
+            $bundle = current($bundles);
+          }
+
+#          dpm($data);
+
+          // hack if really no bundle was supplied... should never be called!
+          if(empty($bundle)) {
+            $entity =  \Drupal\wisski_core\Entity\WisskiEntity::load($entity_id);
+            $bundle = $entity->bundle;
+          }
+#          dpm($entity);
+          $url = 'wisski/navigate/' . $entity_id . '/view';
+#          dpm($bundle);
+
+          // special handling for paths with datatypes - use the value from there for reference
+          // if you don't want this - use disamb directly!
+          if($path->getDatatypeProperty() != "empty") {
+
+            $out[] = array(
+              '#type' => 'link',
+#                 '#title' => $data['target_id'],
+              '#title' => $data['target_id'], //wisski_core_generate_title($entity_id, FALSE, $bundle),
+              '#url' => Url::fromRoute('entity.wisski_individual.canonical', ['wisski_individual' => $entity_id]),
+            );
+            $out[] = [ '#markup' => '</br>' ];
+          } else {
+
+            $out[] = array(
+              '#type' => 'link',
+#                 '#title' => $data['target_id'],
+              '#title' => wisski_core_generate_title($entity_id, FALSE, $bundle),
+              '#url' => Url::fromRoute('entity.wisski_individual.canonical', ['wisski_individual' => $entity_id]),
+              //Url::fromUri('internal:/' . $url . '?wisski_bundle=' . $bundle),
+            );
+            $out[] = [ '#markup' => '</br>' ];
+          }
+        } else {
+          $out[] = array(
+            '#type' => 'item',
+            '#markup' =>  $data['target_id'],
+          );
+          $out[] = [ '#markup' => '</br>' ];
+        }
+
+      }
+    }
+
+
+
+
+
+
+    //Mein urspruenglicher Code speziell fuer die Timeline :
+/*
+    //if (!empty($config['hello_block_name'])) {
+    //  $name = $config['hello_block_name'];
+    //}
+    //else {
+    //  $name = $this->t('to no one');
+    //}
 
     $my_dom = $this->load_my_html('');
 
     $result = array(
-      /*'#markup' => $this->t('Hello @name!', array(
-        '@name' => $name,
-      )),*/
+      //'#markup' => $this->t('Hello @name!', array(
+      //  '@name' => $name,
+      //)),
       //'#children' => 'hallo welt <blink><marquee>HALLO!!!</marquee></blink>', //$my_dom,
       '#children' => $my_dom->saveHTML(),
       '#attached' => array(
@@ -44,11 +292,10 @@ class hello_block extends BlockBase {
       ),
     );
    ///$result['#hello_block']['#attached']['library'] = 'wisski_timeline/example_timeline';
-    $result['#hello_block']['#attached']['library'] = 'wisski_timeline/jquery_timeline';
-    //$result['#hello_block']['dom_element'] = $this->load_my_html_example('');
-    //$result['#attached']['library'][] = 'wisski_timeline/example_timeline';
-    return $result;
-  }
+   //$result['#hello_block']['#attached']['library'] = 'wisski_timeline/jquery_timeline';
+    return $result;*/
+  return $out;
+}
 
    /**
    * {@inheritdoc}
@@ -58,12 +305,40 @@ class hello_block extends BlockBase {
 
     $config = $this->getConfiguration();
 
-    $form['hello_block_name'] = [
+    $timelineblockpbid = "timeline_pathbuilder";
+
+    //TODO: will ich diese Moeglichkeit ueberhaupt?
+    $form['multi_pb'] = [
+      '#type' => 'checkbox',
+      '#title' => 'Use timelineblock with any pathbuilder and adapter',
+      '#default_value' => isset($config['multi_pb']) ? $config['multi_pb'] : 0,
+    ];
+
+    $field_options = array(
+      Pathbuilder::CONNECT_NO_FIELD => $this->t('Do not connect a pathbuilder'),
+      Pathbuilder::GENERATE_NEW_FIELD => $this->t('Create a block specific pathbuilder'),
+    );
+
+    $pbs = \Drupal\wisski_pathbuilder\Entity\WisskiPathbuilderEntity::loadMultiple();
+
+    foreach($pbs as $pb) {
+      $field_options[$pb->id()] = $pb->getName();
+    }
+
+    $form['pathbuilder'] = array(
+      '#type' => 'select',
+      '#title' => $this->t('Pathbuilder'),
+      '#description' => $this->t('What pathbuilder do you want to choose as a source for paths for this timelineblock?'),
+      '#options' => $field_options,
+      '#default_value' => isset($config['pathbuilder']) ? $config['pathbuilder'] : Pathbuilder::GENERATE_NEW_FIELD,
+    );
+
+    /*$form['hello_block_name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Who'),
       '#description' => $this->t('Who do you want to say hello to?'),
       '#default_value' => isset($config['hello_block_name']) ? $config['hello_block_name'] : '',
-    ];
+    ];*/
 
     return $form;
   }
@@ -73,9 +348,29 @@ class hello_block extends BlockBase {
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
     parent::blockSubmit($form, $form_state);
+    $this->configuration['multi_pb'] = $form_state->getValue('multi_pb');
+
+    // if the user said he wants a new one, he gets a new one!
+    if($form_state->getValue('pathbuilder') == Pathbuilder::GENERATE_NEW_FIELD) {
+      // I don't know why the id is hidden there...
+      $block_id = $form_state->getCompleteFormState()->getValue('id');
+      // title can be received normally.
+      $title = $form_state->getValue('label');
+
+      // generate a pb with a nice name - but it is unique for this block due to its id.            
+      $pb = new \Drupal\wisski_pathbuilder\Entity\WisskiPathbuilderEntity(array("id" => 'pb_' . $block_id, "name" => "" . $title . " (Timelineblock)"), "wisski_pathbuilder");
+      $pb->setType("linkblock");	//TODO oder path????
+      $pb->save();
+
+      $this->configuration['pathbuilder'] = $pb->id();
+    } else {
+      $this->configuration['pathbuilder'] = $form_state->getValue('pathbuilder');
+    }
+
+
     //If you have a filedset wrapper around form elemnts => pass array to getValue() instead of field name alone
-    $values = $form_state->getValues();
-    $this->configuration['hello_block_name'] = $values['hello_block_name'];
+    //$values = $form_state->getValues();
+    //$this->configuration['hello_block_name'] = $values['hello_block_name'];
   }
 
 public function load_my_html_first_test($html) {
