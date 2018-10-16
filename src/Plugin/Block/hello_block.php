cf0dc4aa8a970142699208d63a2c11c0174e8f8b
Lisa Marie Dreier <lisa.dreier@fau.de>
2018-10-16 23:23:31 +0200

    Added license note for hello_block.php

--- src/Plugin/Block/hello_block.php
+++ src/Plugin/Block/hello_block.php
@@ -19,6 +19,25 @@ use Drupal\wisski_core\Entity\WisskiEntity;
  *   category = @Translation("Hello World"),
  * )
  */
+ 
+ /*
+This part of the module manages the block that could be used
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/  
+ 
 class hello_block extends BlockBase {
 
   /**
